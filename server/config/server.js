0d6c18f46489205a96f553cfee4b756440bdfbb7
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2023-05-09 13:19:05 -0500

    Changed strapi server.js so admin page can be accessed on deployment.

--- server/config/server.js
+++ server/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
-  url: 'https://blab-times.vercel.app/',
+  url: 'https://hanna-blog.onrender.com',
   app: {
     keys: env.array('APP_KEYS'),
   },
