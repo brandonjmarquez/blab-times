437b583d55ea88a7d4e2873864ba2090b4e232f4
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2023-05-10 13:56:23 -0500

    Changed server.js for render.com.

--- server/config/server.js
+++ server/config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
-  // url: 'https://hanna-blog.onrender.com',
-  url: 'http://127.0.0.1:1338',
+  url: 'https://hanna-blog.onrender.com',
+  // url: 'http://127.0.0.1:1338',
   app: {
     keys: env.array('APP_KEYS'),
   },
