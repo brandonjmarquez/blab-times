406a556abb283c345967afb387fda44783491244
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2023-07-10 20:31:23 -0500

    Fixed new post emails sending when post isn't published.

--- server/src/api/therapy-talk/content-types/therapy-talk/lifecycles.js
+++ server/src/api/therapy-talk/content-types/therapy-talk/lifecycles.js
@@ -11,46 +11,8 @@ module.exports = {
     }
   },
 
-  async afterCreate(event) {    // Connected to "Save" button in admin panel
-    // try{
-    //   console.log(event.result.publishedAt)
-    //   if(event.result.publishedAt) {
-    //     const addRecentPost = await strapi.entityService.create('api::recent-post.recent-post', {
-    //       data: {
-    //         postId: event.result.id,
-    //         api: event.result.api,
-    //         title: event.result.title,
-    //         dateCreated: event.result.publishedAt,
-    //         publishedAt: new Date(),
-    //       }
-    //     })
-
-    //     const entries = await strapi.db.query('api::subscribed.subscribed').findMany();
-
-    //     if(entries.length > 0)
-    //       for(var i = 0; i < entries.length; i++) {
-    //         await strapi.plugins['email'].services.email.send({
-    //           to: entries[i].email,
-    //           from: process.env.SMTP_USERNAME, // e.g. single sender verification in SendGrid
-    //           subject: 'Blab Times has a new post!',
-    //           html: `
-    //             <h1>New Post Alert</h1>
-    //             <p>Check it out at the link below:</p>
-    //             <p><a href="https://blab-times.vercel.app/${event.result.api}/${event.result.id}">${event.result.title}</a></p>
-    //             <br>
-    //             <p><a href="https://blab-times.vercel.app/unsubscribe#${entries[i].email}">Unsubscribe</a></p>
-    //           `,
-    //         });
-    //     }
-    //   }
-    // } catch(err) {
-    //     console.log(err);
-    // }
-  },
-
   async afterUpdate(event) {
     try{
-      console.log(event.result)
       if(event.result.publishedAt) {
         const addRecentPost = await strapi.entityService.create('api::recent-post.recent-post', {
           data: {
@@ -66,7 +28,6 @@ module.exports = {
 
         if(entries.length > 0)
           for(var i = 0; i < entries.length; i++) {
-            console.log(entries[i])
             await strapi.plugins['email'].services.email.send({
               to: entries[i].email,
               from: process.env.SMTP_USERNAME, // e.g. single sender verification in SendGrid
